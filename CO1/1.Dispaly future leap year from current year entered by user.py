1f493fb2dfe8ffe40a1a0edc16edf2616b103a04
Akshay <56751101+taraxdev@users.noreply.github.com>
2022-03-02 23:23:56 +0530

    Delete CO1 directory

--- CO1/1.Dispaly future leap year from current year entered by user.py
+++
@@ -1,7 +0,0 @@
-cyear=int(input("Enter current year:"))
-fyear=int(input("Enter final year:"))
-for cyear in range(cyear,fyear):
-  if(cyear%400==0)or(cyear%4==0)and(cyear%100!=0):
-    print(cyear,"is a leap year")
-  else:
-    print(cyear,"is not a leap year")
